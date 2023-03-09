641abf1a6d48a47ba773093f9e58f05a9ec0d1b7
csais3 <c.sais1986@gmail.com>
2023-03-09 11:33:18 -0700

    updated settings - erased secretd key from file

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-9w7wju%m^%l6$3jn!i+91y1k23p5k*sbyde2ue%(%ln-b1m*y3")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1" # 1 == True
